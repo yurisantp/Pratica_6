0e1490878c4e9e911ec9ac5ad04e0d329ad0a59f
JOAO_VICTOR_LOPES_RAMALHO_TORTA <71900209@OMEGAB30.BARROCA>
2019-06-11 21:53:28 -0300

    Adicionar arquivos de projeto.

---
+++ Pratica_6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("         /---------------------------------------\\");
+            Console.WriteLine("         |Digite uma opção:                       |");
+            Console.WriteLine("         |----------------------------------------|");
+            Console.WriteLine("         |1 - Informar arquivo                    |");
+            Console.WriteLine("         |----------------------------------------|");
+            Console.WriteLine("         |2 - Pesquisar por uma palavra           |");
+            Console.WriteLine("         |----------------------------------------|");
+            Console.WriteLine("         |3 - Imprimir índice em ordem alfabética |");
+            Console.WriteLine("         |----------------------------------------|");
+            Console.WriteLine("         |4 - Imprimir índice em pré-ordem        |");
+            Console.WriteLine("         |----------------------------------------|");
+            Console.WriteLine("         |5 - Imprimir índice em pós-ordem        |");
+            Console.WriteLine("         |----------------------------------------|");
+            Console.WriteLine("         |6 - Sair                                |");
+            Console.WriteLine("         \\---------------------------------------/");
+        }
+    }
+}
